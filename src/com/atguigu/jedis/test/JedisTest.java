5edcfa2e39b7dbf9552c2b0b45086061256e82fe
nyy <nyy@qq.com>
2019-07-08 19:13:47 +0800

    update by local

---
+++ src/com/atguigu/jedis/test/JedisTest.java
@@ -0,0 +1,19 @@
+package com.atguigu.jedis.test;
+
+import org.junit.Test;
+
+import redis.clients.jedis.Jedis;
+
+public class JedisTest {
+	@Test
+	public void test() {
+		//创建客户端对象
+		Jedis jedis = new Jedis("192.168.4.4", 6379, 20000);
+		//使用
+		String result = jedis.ping();
+		System.out.println(result);
+		
+		//关闭客户端
+		jedis.close();
+	}
+}
